2646b9572b10fbef1c6f3f7096bdd6c270c94ade
IceBearVenom <tristansbastian848@gmail.com>
2025-01-30 19:29:32 +0700

    Done(Maybe)

--- script/script.js
+++ script/script.js
@@ -11,8 +11,18 @@ const ResetButton = document.getElementById("reset-button");
 const DisplayResult = document.getElementById("result");
 const DisplayType = document.getElementById("type-result");
 const DisplayDescription = document.getElementById("description-result");
+const DisplayPrevention = document.getElementById("advice");
+// Disease Possibility
+const DiseasePossibility = {
+    "Underweight!": {"Disease": "Being underweight can increase the risk of health problems such as malnutrition, decreased bone density, and problems with the immune system. It can also increase the risk of infection and slow wound healing.", "Prevention": "Increase calorie and protein intake. <br> Eat more often and choose nutritious foods. <br> Do strength training to gain muscle mass."},
+    "Normal": {"Disease": "In general, this category is considered to have lower health risks. Maintaining a BMI in this range is likely to reduce the risk of heart disease, type 2 diabetes and metabolic disorders.", "Prevention": "Maintain a balanced diet. <br> Do regular exercise. <br> Manage stress and get enough sleep."},
+    "Overweight!": {"Disease": "Being overweight increases the risk of heart disease, high blood pressure, type 2 diabetes, sleep disorders (such as sleep apnea), and joint disorders. It may also increase the risk of certain cancers.", "Prevention": "Reduce calories and consume healthy foods. <br> Increase exercise and portion control."},
+    "Obesity!": {"Disease": "Obesity is linked to an increased risk of heart disease, stroke, type 2 diabetes, hypertension, liver disease, joint problems, sleep disorders and some cancers. Obesity can also reduce quality of life and increase the risk of premature death.", "Prevention": "Follow a low-calorie, high-fiber diet. <br> Increase cardio exercise and strength training. <br> Consult a doctor or nutritionist."}
+}
+// Styling Variable
+const ResultSection = document.getElementById("result-section");
 
-// ===================== Function ===========================
+// ===================== Function =========================== 
 // Count BMI
 function CountBMI(event) {
     // Prevent form submission behavior (page refresh)
@@ -21,30 +31,48 @@ function CountBMI(event) {
     // Get the values from the inputs
     const mass = parseFloat(BodyMassInput.value);
     const height = parseFloat(BodyHeightInput.value) / 100; // Convert height to meters
+    const age = parseFloat(AgeInput.value);
 
     // Ensure the inputs are valid
-    if (isNaN(mass) || isNaN(height)) {
+    if (isNaN(mass) || isNaN(height) || isNaN(age)) {
         alert("Please provide valid inputs.");
-        return;
-    }
-
-    // Count BMI
-    var result = mass / (height ** 2);
-    DisplayResult.innerHTML = result.toFixed(2); // Show BMI rounded to two decimal places
-
-    // Result BMI
-    var type;
-    if (result < 18.5) {
-        type = "Underweight!";
-    } else if (result >= 18.5 && result <= 24.9) {
-        type = "Normal";
-    } else if (result >= 25.0 && result <= 29.9) {
-        type = "Overweight!";
     } else {
-        type = "Obesity";
-    }
+        // Count BMI
+        let result = mass / (height ** 2);
+        let ResultSectionColor;
+    
+        // Type Result BMI
+        let type;
+        if (result < 18.5) {
+            type = "Underweight!";
+            ResultSectionColor = "#C5665F"
+        } else if (result >= 18.5 && result <= 24.9) {
+            type = "Normal";
+            ResultSectionColor = "#5FC563"
+        } else if (result >= 25.0 && result <= 29.9) {
+            type = "Overweight!";
+            ResultSectionColor = "#C5665F"
+        } else {
+            type = "Obesity!";
+            ResultSectionColor = "#B91A1A"
+        }
+    
+        // Checking if The Result is Zero
+        if (!isNaN(type) || !isNaN(result)){
+            // Show BMI rounded to two decimal places
+            DisplayResult.innerHTML = result.toFixed(2); 
+            // Display Change
+            DisplayType.innerHTML = type;
+            DisplayDescription.innerHTML = DiseasePossibility[type]["Disease"];
+            DisplayPrevention.innerHTML = DiseasePossibility[type]["Prevention"];
+            ResultSection.style.backgroundColor = ResultSectionColor;
+        }
 
-    DisplayType.innerHTML = type;
+        // Scroll to The Position of Result Section
+        DisplayResult.scrollIntoView({
+            behavior: "smooth"
+        })
+    }
 };
 
 // ===================== Calling ===========================
